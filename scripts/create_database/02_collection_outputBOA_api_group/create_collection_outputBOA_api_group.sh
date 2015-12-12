1e4c3734f9587459caea07d0795cd5ae86386623
Aline Brito <nbrito.aline@gmail.com>
2015-12-12 16:41:18 -0200

    Script para criar coleção com registros de popularidade agrupados.

---
+++ scripts/create_database/02_collection_outputBOA_api_group/create_collection_outputBOA_api_group.sh
@@ -0,0 +1,21 @@
+##
+#	Cria a coleção outputBOA_api_group, onde os registros de popularidade são agrupados.
+#
+#	Exemplo da estrutura criada:
+# {
+# 		"_id" : "java.util.List",
+# 		"value" : {
+# 			"OccurrenceFile" : 1.0000000000000000,
+# 			"OccurrenceProject" : 1.0000000000000000
+# 		}
+# }
+#
+#	Onde:
+#		_id: nome da API;
+#		value.OccurrenceFile: quantidade de arquivos onde a API foi encontrada.
+#		value.OccurrenceFile: quantidade de projetos distintos onde a API foi encontrada.
+#	
+#	Para executar utilize: sh importsByProjectAndFile.sh
+#
+
+mongo < create_collection_outputBOA_api_group.js
